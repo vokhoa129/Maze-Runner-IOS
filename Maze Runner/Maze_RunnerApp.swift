a99f79a18fca93f1f92ae69f8389081b56d04171
Khoa Võ <vochauanhkhoa@gmail.com>
2022-01-20 10:43:58 +0700

    Initial Commit

---
+++ Maze Runner/Maze_RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maze_RunnerApp.swift
+//  Maze Runner
+//
+//  Created by NGUYEN Tuan Anh on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Maze_RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
